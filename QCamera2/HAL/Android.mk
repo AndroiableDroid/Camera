628d70abdcd76eb081f4ebdbcfd9e82851593de8
Mohd Faraz <mohd.faraz.abc@gmail.com>
2019-06-07 07:05:17 +0000

    Revert "QCamera2: Disable CLANG"
    This reverts commit bea87c7a3c113a56b8a16a6814762190823c21a8.
    
    Signed-off-by: Mohd Faraz <mohd.faraz.abc@gmail.com>

--- QCamera2/HAL/Android.mk
+++ QCamera2/HAL/Android.mk
@@ -71,7 +71,6 @@ LOCAL_STATIC_LIBRARIES := android.hardware.camera.common@1.0-helper
 
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
-LOCAL_CLANG := false
 LOCAL_32_BIT_ONLY := true
 LOCAL_VENDOR_MODULE := true
 LOCAL_MODULE_TAGS := optional
